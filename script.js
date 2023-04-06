2cb4d13a9188593e679341b4bbb261f9c3b2b718
hamidu sodiq <hamidusodiq2@gmail.com>
2023-04-06 18:31:06 +0100

    Handled submission to cart

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ class App {
   constructor() {
     console.log(window.innerWidth);
     mobileNavBtn.addEventListener("click", this.mobileNavHandler);
+    cartSection.addEventListener("click", this.cartSubmitHandler.bind(this));
   }
 
   //toggle mobile navigation
@@ -57,6 +58,66 @@ class App {
       mobileNavEl.setAttribute("data-visible", true);
     } else {
       mobileNavEl.setAttribute("data-visible", false);
+    }
+  }
+
+  //handle submission of purchases to card
+  cartSubmitHandler(e) {
+    e.preventDefault();
+    // console.log(e.target);
+
+    //increase quantity
+    if (e.target.classList.contains("add-button")) {
+      cartAmount.value >= 0 ? cartAmount.value++ : (cartAmount.value = 0);
+    }
+
+    //decrease quantity
+    if (e.target.classList.contains("minus-button")) {
+      cartAmount.value > 0 ? cartAmount.value-- : (cartAmount.value = 0);
+    }
+
+    //add to cart
+    if (e.target.closest(".cart-button")) {
+      if (cartAmount.value > 0) {
+        this.amount = Number(cartAmount.value);
+        this.addToCart();
+        cartAmount.value = 0;
+      } else {
+        window.alert("invalid quantity");
+      }
+    }
+  }
+
+  addToCart() {
+    let item = `
+    <div class="cart-items ">
+      <div class="cart-item">
+        <img src="./images/image-product-1-thumbnail.jpg" alt="" />
+        <div class="cart-item-desc">
+          <p>Fall Limited Edition Sneakers</p>
+          <p>$125.00 * ${this.amount} <b>$${125.0 * this.amount}</b></p>
+        </div>
+        <img src="./images/icon-delete.svg" alt="" class="icon-delete"/>
+      </div>
+      <div class="cart-items-button">
+        <button class='checkout-button'>checkout</button>
+      </div>
+    </div>
+        `;
+
+    let countEl = `
+    <div class="cart-count">${this.amount}</div>
+    `;
+    console.log("cart");
+    cartContainer.innerHTML = "";
+    cartContainer.insertAdjacentHTML("afterbegin", item);
+
+    if (cartIcon.nextElementSibling) {
+      cartIcon.nextElementSibling.remove();
+      cartIcon.parentElement.insertAdjacentHTML("beforeend", countEl);
+    } else {
+      console.log("doesnt exist");
+      cartIcon.parentElement.insertAdjacentHTML("beforeend", countEl);
     }
   }
 }
